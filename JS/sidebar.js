bb5bcb8e8282a810d8f0484fb132ec8ebb6af576
GZPavlov <GZPavlov23@codingburgas.bg>
2025-06-06 18:38:24 +0300

    Make changes to sidebar

--- JS/sidebar.js
+++ JS/sidebar.js
@@ -1,6 +1,16 @@
 const sidebar = document.querySelector(".sidebar");
 const sideBarToggler = document.querySelector(".sidebarToggler")
 
+document.addEventListener("DOMContentLoaded", () => {
+    const isCollapsed = localStorage.getItem("sidebar-collapsed") === "true";
+    if (isCollapsed) {
+        sidebar?.classList.add("collapsed");
+    }
+});
+
+
 sideBarToggler.addEventListener("click", () => {
     sidebar?.classList.toggle("collapsed");
+    const isNowCollapsed = sidebar?.classList.contains("collapsed");
+    localStorage.setItem("sidebar-collapsed", isNowCollapsed);
 });
